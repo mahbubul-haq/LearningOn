52c559482cada83d934466c9ff32abdcb1d26467
mahbubul-haq <mdmahbubulhaque99@gmail.com>
2026-04-02 13:37:21 +0600

    Fixed deploy bug

--- Backend/src/routes/admin.js
+++ Backend/src/routes/admin.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { login } from "../controllers/admin.auth.js";
-import { getUnpublishedCourses } from "../controllers/course.search.js";
-import { updateStatus } from "../controllers/course.update.js";
+import { getUnpublishedCourses } from "../controllers/courses/course.query.controller.js";
+import { updateStatus } from "../controllers/courses/course.controller.js";
 import verifyAdminToken from "../middlewares/adminAuth.js";
 
 const router = Router();
